2111a854d860b2ddf9dc06431ab6d5fd810968b9
johnythomas <johnythomas007@gmail.com>
2018-04-08 18:20:52 +0530

    Added routing and made create contact coponent structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ListContacts from "./ListContacts";
+import { Route } from "react-router-dom";
 import * as ContactsAPI from "./utils/ContactsAPI";
+import CreateContact from "./CreateContact";
 
 class App extends Component {
   state = {
@@ -26,10 +28,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListContacts
+              contacts={this.state.contacts}
+              onDeleteContact={this.removeContact}
+            />
+          )}
         />
+        <Route path="/create" component={CreateContact} />
       </div>
     );
   }
